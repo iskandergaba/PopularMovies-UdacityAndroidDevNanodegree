476271d4ffc147caec720adc7c77dbacc2c3cddd
Alex Gaba <iskandergaba@gmail.com>
2016-05-27 14:28:01 +0100

    Picasso downloads and displays images on a GridView

--- app/src/main/java/com/example/alexgaba/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/alexgaba/popularmovies/MainActivity.java
@@ -1,7 +1,24 @@
 package com.example.alexgaba.popularmovies;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +26,80 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] mThumbIds = {
+                "http://i.imgur.com/rFLNqWI.jpg",
+                "http://i.imgur.com/C9pBVt7.jpg",
+                "http://i.imgur.com/rT5vXE1.jpg",
+                "http://i.imgur.com/aIy5R2k.jpg",
+                "http://i.imgur.com/MoJs9pT.jpg",
+                "http://i.imgur.com/S963yEM.jpg",
+                "http://i.imgur.com/rLR2cyc.jpg",
+                "http://i.imgur.com/SEPdUIx.jpg",
+                "http://i.imgur.com/aC9OjaM.jpg",
+                "http://i.imgur.com/76Jfv9b.jpg",
+                "http://i.imgur.com/fUX7EIB.jpg",
+                "http://i.imgur.com/syELajx.jpg",
+                "http://i.imgur.com/COzBnru.jpg",
+                "http://i.imgur.com/Z3QjilA.jpg",
+        };
+
+        GridView gridView = (GridView) findViewById(R.id.movies_gridview);
+        gridView.setAdapter(new ImageAdapter(this, mThumbIds));
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(MainActivity.this, "" + position,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    public class ImageAdapter extends BaseAdapter {
+        private Context mContext;
+        String[] mThumbIds;
+
+        public ImageAdapter(Context c, String[] mThumbIds) {
+            mContext = c;
+            this.mThumbIds = mThumbIds;
+        }
+
+        @Override
+        public int getCount() {
+            return mThumbIds.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        // create a new ImageView for each item referenced by the Adapter
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ImageView imageView;
+            if (convertView == null) {
+                // if it's not recycled, initialize some attributes
+                imageView = new ImageView(mContext);
+                imageView.setLayoutParams(new GridView.LayoutParams(GridView.AUTO_FIT, 1024));
+                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            } else {
+                imageView = (ImageView) convertView;
+            }
+
+            Picasso
+                    .with(mContext)
+                    .load(mThumbIds[position])
+                    .into(imageView);
+
+            return imageView;
+        }
     }
 }
